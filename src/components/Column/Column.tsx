aece0fc8e633e5f5369c491df1c561a9017759bb
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2023-11-18 22:46:42 +0100

    feat/addTodo - added btn

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
 
-import { Center, Container} from '@chakra-ui/react'
+import { Button, Center, Container} from '@chakra-ui/react'
 import Icons from '../Icons/Icons'
 import { IconContext } from "react-icons"
 interface ColumnProps {
@@ -16,6 +16,7 @@ export const Column:FC<ColumnProps> = ({ text, weather_code }) => {
           <IconContext.Provider value={{ size: "5rem"}}>
             <Icons weatherCode={weather_code} />
           </IconContext.Provider>
+          <Button colorScheme='gray'>Add a todo for this day</Button>
         </Center>
     </Container>
   )
